610b9b5ccda7c31aac528629e2a62a0ee3e1432a
NguyenTuan200609 <gaulegend06@gmail.com>
2025-12-18 23:55:16 +0700

    sea

---
+++ PHT 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] agrs)
+    {
+        Console.WriteLine("Chào mừng đến với C#!");
+        string ten = "Nguyễn Văn A"; // Chuỗi ký tự
+        int tuoi = 20; // Số nguyên
+        double diem = 8.5; // Số thực
+        Console.WriteLine("Tên: " + ten);
+        Console.WriteLine("Tuổi: " + tuoi);
+        Console.WriteLine("Điểm: " + diem);
+        Console.WriteLine($"Thông tin: {ten}, tuổi {tuoi}, điểm {diem}");
+
+    }
+}
